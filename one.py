7fa4570d314603a0c90e84af4f72cd47442d517a
Nirdosh Kushwaha <148872957+Nirdosh2004@users.noreply.github.com>
2024-01-28 11:48:57 +0530

    Create one.py

---
+++ one.py
@@ -0,0 +1,16 @@
+# a = int(input("enter two values : "))
+# b = int(input("enter second value : "))
+# print("sum of given values are : " , a+b)
+
+# side = float(input("Enter side of a square : "))
+# print("Area of square is : " , side**2)
+
+# first = float(input("Enter first number : "))
+# second = float(input("enter second number : "))
+# avg = (first + second)/2
+# print("avergae of given numbers : " , avg)
+
+first = int(input("Enter first number : "))
+second = int(input("Enter second number : "))
+print(first>=second)  #it returns true or false
+print(second>=first)
