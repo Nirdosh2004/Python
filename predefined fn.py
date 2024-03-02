6e687f47cc376a16938da06b62f6e360361b9118
Nirdosh Kushwaha <148872957+Nirdosh2004@users.noreply.github.com>
2024-03-02 07:09:28 +0530

    Create predefined fn.py

---
+++ predefined fn.py
@@ -0,0 +1,24 @@
+# students = ["Nirdosh" , 20 , "Gorakhpur"]
+# print(type(students))
+# print(students[0])
+# students[0] = "Rishabh Kushwaha"
+# print(students[0])
+# print(type(students[0]))
+# print(students[0:2])  #sublist -> list ka tukda 
+
+list = [2,3,1]
+list.append(5)
+print(list[0:])
+list.sort()
+print(list[0:])
+list.sort(reverse = True)
+print(list[0:])
+list.reverse()
+print(list[0:])
+list.insert(2 , 44)
+print(list[0:])
+
+list.remove(1)
+print(list[0:])
+list.pop(3)
+print(list[0:])
